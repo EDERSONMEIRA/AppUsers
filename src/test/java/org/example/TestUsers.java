ff39256ee29fa6b95f791d088a77d4f6bb38980c
EDERSONMEIRA <ederson.ti.qi@gmail.com>
2020-08-18 18:17:35 -0300

    Inicializando Projeto AppUsers

---
+++ src/test/java/org/example/TestUsers.java
@@ -0,0 +1,94 @@
+package org.example;
+
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+public class TestUsers {
+    @BeforeClass
+    public static  void setup(){
+        RestAssured.baseURI ="https://randomuser.me/";
+        RestAssured.basePath = "/api";
+    }
+
+    @Test
+    public void retornarVinteUsuario(){
+
+        given()
+                .param("results",20)
+                .when()
+                .get("")
+                .then()
+                .body("info.results", Matchers.is(20))
+        ;
+
+    }
+
+    @Test
+    public void retornarUsuarioBrasileiro(){
+
+        given()
+                .param("nat","BR")
+                .when()
+                .get("")
+                .then()
+                .statusCode(200)
+                .body("results[0].nat", Matchers.is("BR"))
+        ;
+
+    }
+
+    @Test
+    public void retornarUsuarioPaginado(){
+
+        given()
+                .param("page",3)
+                .param("results",1)
+                .when()
+                .get("")
+                .then()
+                .body("info.results", Matchers.is(1))
+                .body("info.page",Matchers.is(3))
+        ;
+
+    }
+
+    @Test
+    public void retornarNomeEmail(){
+
+        given()
+                .param("inc","name,email")
+                .when()
+                .get("")
+                .then()
+                .statusCode(200)
+                .body("results[0]", Matchers.hasKey("name"))
+                .body("results[0]", Matchers.hasKey("email"))
+        ;
+    }
+
+    @Test
+    public void retornarUsuarioBrUsEsCa(){
+
+        List<String> nascionalidades = Arrays.asList("BR","US","ES","CA");
+
+        String nat =
+                given()
+                        .param("nat","BR,US,ES,CA")
+                        .when()
+                        .get("")
+                        .then()
+                        .statusCode(200)
+                        .extract().path("results[0].nat")
+                ;
+        Assert.assertThat(nascionalidades, Matchers.hasItem(nat));
+
+    }
+}
